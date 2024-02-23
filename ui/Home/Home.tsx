9e74b341f041852211fa57e33a4631ab391b4367
issa2580 <issadiop2580@gmail.com>
2024-02-23 07:52:38 +0000

    add blog link

--- ui/Home/Home.tsx
+++ ui/Home/Home.tsx
@@ -1,19 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from "react";
 
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import TypewriterComponent from 'typewriter-effect'
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TypewriterComponent from "typewriter-effect";
 
-import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown'
-import { Link } from '@mui/material'
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import { Link } from "@mui/material";
 
 const heading = {
-  fontFamily: 'Edu TAS Beginner',
+  fontFamily: "Edu TAS Beginner",
   fontWeight: 900,
-  fontStyle: 'normal',
-}as const
-
+  fontStyle: "normal",
+} as const;
 
 const Home = () => {
   const [showArrow, setShowArrow] = useState(false);
@@ -27,157 +26,172 @@ const Home = () => {
       }
     };
 
-    window.addEventListener('scroll', scrollListener);
-    return () => window.removeEventListener('scroll', scrollListener)
-  }, [])
+    window.addEventListener("scroll", scrollListener);
+    return () => window.removeEventListener("scroll", scrollListener);
+  }, []);
   return (
     <Box
-      id = 'home'
+      id="home"
       sx={{
-        display:'flex',
-        flexDirection: 'column',
-        justifyContent:'center',
-        alignItems:'center',
-        pt: {xs: '50px', md: '100px'},
-        minHeight: {xs: '80vh', md: '60vh', lg: '95vh'},
-        gap: '30px',
-      }}>
-      <Box 
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        pt: { xs: "50px", md: "100px" },
+        minHeight: { xs: "80vh", md: "60vh", lg: "95vh" },
+        gap: "30px",
+      }}
+    >
+      <Box
         sx={{
-          display:'flex',
-        }}>
+          display: "flex",
+        }}
+      >
         <Typography
           sx={{
             ...heading,
-            fontSize: {xs: '30px', md: '50px'}
-          }}>
-          Hi There! {" "}
+            fontSize: { xs: "30px", md: "50px" },
+          }}
+        >
+          Hi There!{" "}
         </Typography>
         <Box
-          component = 'div'
+          component="div"
           sx={{
-            fontSize: {xs: '20px', md: '50px'}
+            fontSize: { xs: "20px", md: "50px" },
           }}
-          >
-        👋
+        >
+          👋
         </Box>
       </Box>
-      <Box 
+      <Box
         sx={{
-          display:'flex',
-        }}>
+          display: "flex",
+        }}
+      >
         <Typography
           sx={{
             ...heading,
-            fontSize: {xs: '30px', md: '50px'}
-          }}>
-          I am Rootkit {" "}
+            fontSize: { xs: "30px", md: "50px" },
+          }}
+        >
+          I am Rootkit{" "}
         </Typography>
         <Box
-          component = 'div'
+          component="div"
           sx={{
-            fontSize: {xs: '20px', md: '50px'}
+            fontSize: { xs: "20px", md: "50px" },
           }}
-          >
-        🤷‍♂️
+        >
+          🤷‍♂️
         </Box>
       </Box>
-      <Box 
+      <Box
         sx={{
-          display:'flex',
-        }}>
+          display: "flex",
+        }}
+      >
         <Typography
           sx={{
             ...heading,
-            fontSize: {xs: '20px', md: '50px'}
-          }}>
-          Fullstack web developer living in  {" "}
+            fontSize: { xs: "20px", md: "50px" },
+          }}
+        >
+          Fullstack web developer living in{" "}
         </Typography>
         <Box
-          component = 'div'
+          component="div"
           sx={{
             pt: 1,
-            fontSize: {xs: '20px', md: '50px'}
+            fontSize: { xs: "20px", md: "50px" },
           }}
-          >
-        🇸🇳
+        >
+          🇸🇳
         </Box>
       </Box>
-    <Box>
-      <Typography
-        sx={{
-          ...heading, 
-          color: '#64ffda', 
-          fontSize: {xs: '20px', md: '50px'}
-        }}>
-      <TypewriterComponent
-        options={{
-          strings: [
-            'Software Developer 👨‍💻',
-            'MERN Stack Developer 👨‍💻',
-            'Python Django Developer 👨‍💻',
-            'Open Source Contributor 👨‍💻',
-            'Freelancer 👨‍💻',
-          ],
-          autoStart: true,
-          loop: true,
-          deleteSpeed: 50
-        }}
-      />
-      </Typography>
-    </Box>
-    <Box sx={{pt: '50px'}}>
-      <Button
-      sx={{
-        color: '#64ffda',
-        display: 'flex',
-        border: '1px solid',
-        borderWidth: '2px',
-        padding: '10px',
-        width: '200px',
-        '&:hover': {
-          color: 'whitesmoke',
-          borderColor: '#64ffda',
-          margin: '0 auto 5px',
-          width: '200px',
-          boxShadow: '5px 5px 0 0 #64ffda'
-        }
-      }}>
+      <Box>
         <Typography
           sx={{
-            fontFamily: 'Edu TAS Beginner',
-            textTransform: 'capitalize'
-          }}>
-            Check out my blog
+            ...heading,
+            color: "#64ffda",
+            fontSize: { xs: "20px", md: "50px" },
+          }}
+        >
+          <TypewriterComponent
+            options={{
+              strings: [
+                "Software Developer 👨‍💻",
+                "MERN Stack Developer 👨‍💻",
+                "Python Django Developer 👨‍💻",
+                "Open Source Contributor 👨‍💻",
+                "Freelancer 👨‍💻",
+              ],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 50,
+            }}
+          />
         </Typography>
-    </Button>
-    </Box>
-    <Link 
-      sx={{textDecoration: 'none'}}
-      href='#about'>
-      <Box
-      className={`arrow-container ${showArrow ? 'show' : ''}`}
+      </Box>
+      <Box sx={{ pt: "50px" }}>
+        <Button
           sx={{
-            position: 'relative',
-            marginTop: '50px',
-            cursor: 'pointer', 
-            transition: 'opacity 0.3s ease',
+            color: "#64ffda",
+            display: "flex",
+            border: "1px solid",
+            borderWidth: "2px",
+            padding: "10px",
+            width: "200px",
+            "&:hover": {
+              color: "whitesmoke",
+              borderColor: "#64ffda",
+              margin: "0 auto 5px",
+              width: "200px",
+              boxShadow: "5px 5px 0 0 #64ffda",
+            },
+          }}
+        >
+          <Link
+            sx={{ textDecoration: "none" }}
+            href="rootkit-five.vercel.app/"
+            target="_blank"
+          >
+            <Typography
+              sx={{
+                fontFamily: "Edu TAS Beginner",
+                textTransform: "capitalize",
+                color: "#64ffda",
+              }}
+            >
+              Check out my blog
+            </Typography>
+          </Link>
+        </Button>
+      </Box>
+      <Link sx={{ textDecoration: "none" }} href="#about">
+        <Box
+          className={`arrow-container ${showArrow ? "show" : ""}`}
+          sx={{
+            position: "relative",
+            marginTop: "50px",
+            cursor: "pointer",
+            transition: "opacity 0.3s ease",
           }}
           onClick={() => {
-          //  tst 
+            //  tst
           }}
         >
           <KeyboardDoubleArrowDownIcon
             sx={{
-              fontSize: '75px',
-              color: '#64ffda',
-              animation: 'bounce 1s infinite',
+              fontSize: "75px",
+              color: "#64ffda",
+              animation: "bounce 1s infinite",
             }}
           />
-      </Box>
+        </Box>
       </Link>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
